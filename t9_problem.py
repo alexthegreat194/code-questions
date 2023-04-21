6bdc9236949f254aec85cc5b0ecd0b661e0d6ad6
alexthegreat194 <alexander.gentry@aol.com>
2023-04-20 23:47:26 -0700

    t9 problem

---
+++ t9_problem.py
@@ -0,0 +1,16 @@
+buttons = {
+    '2': 'abs',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+}
+
+def t9_letters(i:str):
+    pass
+
+if __name__ == "__main__":
+    print(t9_letters("23"))

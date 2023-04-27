e9bca84e1f0d032597dc9d07c811734429b1dce1
alexthegreat194 <alexander.gentry@aol.com>
2023-04-26 20:11:00 -0700

    t9

--- t9_problem.py
+++ t9_problem.py
@@ -1,5 +1,5 @@
 buttons = {
-    '2': 'abs',
+    '2': 'abc',
     '3': 'def',
     '4': 'ghi',
     '5': 'jkl',
@@ -9,8 +9,20 @@ buttons = {
     '9': 'wxyz'
 }
 
-def t9_letters(i:str):
-    pass
+class Solution:
+    def letterCombinations(self, digits: str, l: List[str] = []) -> List[str]:
+        if digits == "":
+            return l
 
-if __name__ == "__main__":
-    print(t9_letters("23"))
+        big_list = []
+        letter_combo = list(buttons[digits[0]])
+        if len(l) == 0:
+            big_list = letter_combo
+        else:
+            for i in l:
+                letter_list = []
+                for letter in list(buttons[digits[0]]):
+                    letter_list.append(i + letter)
+                big_list.extend(letter_list)
+
+        return self.letterCombinations(digits[1:], big_list)

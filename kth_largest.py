43aa7634448b237232cdf6e7d884752de3de551b
alexthegreat194 <alexander.gentry@aol.com>
2023-04-27 18:53:13 -0700

    kth problem

---
+++ kth_largest.py
@@ -0,0 +1,6 @@
+#Given an array a of n numbers and a count k find the k largest values in the array a.
+#Example: a=[5, 1, 3, 6, 8, 2, 4, 7], k=3  ⇒  [6, 8, 7]
+
+def kth_largest(a, k):
+    a.sort()
+    return a[-k:]
